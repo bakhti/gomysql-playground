575fc26fb68cb5756d65f6eebb9d2c76b8327651
Bakhtiyor Aripov <bakhti@gmail.com>
2020-03-24 19:22:07 +0100

    Connect to the source database

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/bakhti/gomysql-playground
 go 1.13
 
 require (
+	github.com/go-sql-driver/mysql v1.5.0
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/sirupsen/logrus v1.2.0
 	github.com/spf13/cobra v0.0.6
--- pkg/validators/data_validator.go
+++ pkg/validators/data_validator.go
@@ -1,7 +1,10 @@
 package validators
 
 import (
+	"database/sql"
 	"github.com/sirupsen/logrus"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type Validator struct {
@@ -21,6 +24,12 @@ func (v *Validator) Run() error {
 	//err := errors.New("Error running the validator")
 	//v.logger.WithError(err).Error(err.Error())
 	//return err
+	var db *sql.DB
+	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:29291)/validator")
+	defer db.Close()
+	if err != nil {
+		v.logger.Fatalf("err: %v", err)
+	}
 
 	v.logger.WithField("from", "validator").Info("Hello I am the validator!")
 
